970c90a405e35305a53b053594276b069af2c630
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-11-20 14:52:02 +0100

    Update install_promtail.sh

--- scripts/install_promtail.sh
+++ scripts/install_promtail.sh
@@ -4,7 +4,6 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 # Updates the list of available packages
 sudo apt-get update
-sudo apt-get update
 sudo apt-get install promtail
 sudo systemctl enable promtail
 sudo wget -O /etc/promtail/config.yml  wget wget https://raw.githubusercontent.com/grafana/loki/main/clients/cmd/promtail/promtail-local-config.yaml
